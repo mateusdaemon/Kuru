87e5031d34294a3461fdf5f7c1e18d5f425e8516
mateusdaemon <mateusjdm@gmail.com>
2025-04-11 14:08:55 -0300

    adjusting aim icon to be centered

---
+++ KuruProject/Assets/Assets/MyAssets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
